b98a392aa6554c1cb4b12a4912350c11ba31f8ca
Adam Palmer <theadampalmer@gmail.com>
2020-07-10 21:06:33 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,11 +1,11 @@
 
 
 def using_until
-  phrase = "Wingardium Leviosa" 
+  phrase = "Wingardium Leviosa"
   levitation_force = 6
-      puts "#{phrase}"
-      until levitation_force == 10
-   end
+	  until levitation_force == 10
+	    puts "#{phrase}"
+	    levitation_force += 1
+    end
 end
-
 using_until
